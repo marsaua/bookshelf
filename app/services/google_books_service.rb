41ec193871464e0732638df6ed367cd70e0cc98d
marsaua001 <marsaua001@gmail.com>
2026-05-04 09:12:06 +0300

    fix: google books

--- app/services/google_books_service.rb
+++ app/services/google_books_service.rb
@@ -7,35 +7,40 @@ class GoogleBooksService
   API_URL = 'https://www.googleapis.com/books/v1/volumes'
 
   def self.search(query)
-    uri = URI(API_URL)
-    uri.query = URI.encode_www_form(
-      q: query,
-      key: ENV.fetch('GOOGLE_BOOKS_API_KEY', nil),
-      maxResults: 5,
-      langRestrict: 'en'
-    )
+  uri = URI(API_URL)
+  uri.query = URI.encode_www_form(
+    q: query,
+    key: ENV.fetch('GOOGLE_BOOKS_API_KEY', nil),
+    maxResults: 5,
+    langRestrict: 'en'
+  )
 
-    response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-      http.get(uri.request_uri)
-    end
-
-    Rails.logger.debug "RESPONSE CODE: #{response.code}"
-    Rails.logger.debug "RESPONSE BODY: #{response.body}"
-
-    data = JSON.parse(response.body)
-
-    data['items']&.map do |item|
-      info = item['volumeInfo']
-      {
-        title: info['title'],
-        author: info['authors']&.first,
-        description: info['description'],
-        image: info['imageLinks']&.dig('thumbnail'),
-        category: info['categories']&.first,
-        publisher: info['publisher'],
-        language: info['language'],
-        pageCount: info['pageCount']
-      }
-    end
+  response = Net::HTTP.start(uri.host, uri.port, use_ssl: true, open_timeout: 3, read_timeout: 5) do |http|
+    http.get(uri.request_uri)
   end
+
+  unless response.is_a?(Net::HTTPSuccess)
+    Rails.logger.error "Google Books API error: #{response.code}"
+    return []
+  end
+
+  data = JSON.parse(response.body)
+
+  (data['items'] || []).map do |item|
+    info = item['volumeInfo']
+    {
+      title: info['title'],
+      author: info['authors']&.first,
+      description: info['description'],
+      image: info['imageLinks']&.dig('thumbnail'),
+      category: info['categories']&.first,
+      publisher: info['publisher'],
+      language: info['language'],
+      page_count: info['pageCount']
+    }
+  end
+rescue JSON::ParserError, Net::OpenTimeout, Net::ReadTimeout => e
+  Rails.logger.error "GoogleBooksService failed: #{e.class} #{e.message}"
+  []
+end
 end
